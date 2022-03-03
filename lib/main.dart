dde98cd309aa20e01a922fe2d17e9b9228a14f0c
piotrekkruk <piotrekkruk@icloud.com>
2022-03-03 15:02:51 +0100

    MagicBall 0.0.1 Final

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,49 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.blue,
-          appBar: AppBar(
-            title: Text('Ask Me Anything'),
-            backgroundColor: Colors.blue.shade900,
-            padding: const EdgeInsets.only(left: 20.0),
-          ),
-        ),
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int picture = Random().nextInt(5) + 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              setState(() {
+                picture = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset('images/ball$picture.png'),
+          ),
+        )
+      ],
+    ));
+  }
+}
